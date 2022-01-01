e12f2eaad6355713e0a514a98fecc7ebe8f51ea9
wisnermarques <76441538+wisnermarques@users.noreply.github.com>
2022-01-01 19:12:54 -0300

    Implementação da função de adição

---
+++ src/js/calculadora.js
@@ -0,0 +1,21 @@
+var Calculadora = {
+    adicionar: function(num1, num2) {
+        if(isNaN(num1) || isNaN(num2)) {
+            return 0
+        }
+        num1 = parseFloat(num1)
+        num2 = parseFloat(num2)
+        return num1 + num2
+    },
+    subtrair: function(num1, num2) {
+        return 0
+    },
+    dividir: function(num1, num2) {
+        return 0
+    },
+    multiplicar: function(num1, num2) {
+        return 0
+    },
+}
+
+module.exports = Calculadora
