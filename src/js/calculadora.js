3ade2a1da1e066c45865e43276cf52cf2ae4c1c4
wisnermarques <76441538+wisnermarques@users.noreply.github.com>
2022-01-02 09:46:52 -0300

    Refatoração do objeto calculadora

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -1,33 +1,54 @@
 var Calculadora = {
+    ADICAO: '+',
+    SUBTRACAO: '-',
+    DIVISAO: '/',
+    MULTIPLICACAO: '*',
+
     adicionar: function(num1, num2) {
-        if(isNaN(num1) || isNaN(num2)) {
-            return 0
-        }
-        num1 = parseFloat(num1)
-        num2 = parseFloat(num2)
-        return num1 + num2
+        return this.calcular(num1, num2, this.ADICAO)
     },
     subtrair: function(num1, num2) {
-        if(isNaN(num1) || isNaN(num2)) {
-            return 0
-        }
-        return num1 - num2
+        return this.calcular(num1, num2, this.SUBTRACAO)
     },
     dividir: function(num1, num2) {
         if(num2 == 0) {
             return 'Erro'
         }
-        if(isNaN(num1) || isNaN(num2)) {
-            return 0
-        }
-        return num1 / num2
+        
+        return this.calcular(num1, num2, this.DIVISAO)
     },
     multiplicar: function(num1, num2) {
+        return this.calcular(num1, num2, this.MULTIPLICACAO)
+    },
+
+    calcular: function(num1, num2, operacao) {
+        
+        var resultado = 0
+
+        num1 = parseFloat(num1)
+        num2 = parseFloat(num2)
         if(isNaN(num1) || isNaN(num2)) {
             return 0
         }
-        return num1 * num2
-    },
+        switch(operacao) {
+            case this.ADICAO: 
+                resultado = num1 + num2
+                break
+            case this.SUBTRACAO: 
+                resultado = num1 - num2
+                break
+            case this.DIVISAO: 
+                resultado = num1 / num2
+                break
+            case this.MULTIPLICACAO: 
+                resultado = num1 * num2
+                break
+            default:
+                resultado = 0
+                break
+        }
+        return resultado
+    }
 }
 
 module.exports = Calculadora
